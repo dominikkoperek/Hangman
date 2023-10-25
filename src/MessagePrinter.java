900c21e3679ed188fade9f6918cc78bb45345b82
Dominik <142162555+dominikkoperek@users.noreply.github.com>
2023-10-25 10:53:27 +0200

    Delete src directory

--- src/MessagePrinter.java
+++
@@ -1,9 +0,0 @@
-public class MessagePrinter {
-    public void printLine(String txt) {
-        System.out.println(txt);
-    }
-    public void print(String txt) {
-        System.out.print(txt);
-    }
-
-}
