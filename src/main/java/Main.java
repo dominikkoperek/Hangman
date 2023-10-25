773eea02a80ef4e07fa157ece64d97cf1a50dfa8
Dominik <142162555+dominikkoperek@users.noreply.github.com>
2023-10-25 10:53:48 +0200

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        GameService gameService = new GameService();
+        gameService.startGame();
+    }
+}
